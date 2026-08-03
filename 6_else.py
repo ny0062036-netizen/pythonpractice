56848fb40109886e6753cef69bd6727ef74b0c75
nikku <ny0062036@gmail.com>
2026-08-03 12:16:06 +0530

    Add files via upload

--- 6_else.py
+++ 6_else.py
@@ -30,3 +30,21 @@
 #  print("no you cannot vote.")
 #  print("no you cannot vote.")
 
+
+# age = 20
+
+# if age >= 25:
+   # print("yes you can vote.")
+   # print("yes you can vote.")
+
+# else:
+   # print("no you cannot vote.")
+   # print("no you cannot vote.")
+
+# score = int(input("enter your math's number: "))
+
+# if score > 33:
+   # print("you have passed in the exam")
+
+# else:
+  #  print("you failed in practice and try again")    
