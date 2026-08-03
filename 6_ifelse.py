3bd6561a6628e9089330b6b1e5a536502f8ba926
nikku <ny0062036@gmail.com>
2026-08-03 13:08:57 +0530

    Add files via upload

---
+++ 6_ifelse.py
@@ -0,0 +1,96 @@
+# a = input("enter your age: ")
+# a = int(a)
+
+# print(a > 18)
+
+
+# a = input("enter your age: ")
+# a = int(a)
+# print(a < 20)
+
+# a = input("enter a number: ")
+# a = int(a)
+# print(a > 0)
+
+# a = input("enter a number: ")
+# a = int(a)
+#print(a > 6)
+
+# a = int(input("enter your age: "))
+# print(a < 54)
+
+# age = 15
+
+# if age >= 20:
+
+  # print("yes you  can vote.")
+  # print("yes you  can vote.")
+
+# else:
+#  print("no you cannot vote.")
+#  print("no you cannot vote.")
+
+
+# age = 20
+
+# if age >= 25:
+   # print("yes you can vote.")
+   # print("yes you can vote.")
+
+# else:
+   # print("no you cannot vote.")
+   # print("no you cannot vote.")
+
+# score = int(input("enter your math's number: "))
+
+# if score > 33:
+   # print("you have passed in the exam")
+
+# else:
+  #  print("you failed in practice and try again")    
+
+# a = "i"
+
+# if a == "a":
+  # print("yes")
+
+# elif a == "e":
+  # print("yes")
+
+# elif a == "i":
+  # print("yes")
+
+# elif a == "o":
+  # print("yes") 
+
+# elif a == "u":
+  # print("yes")
+
+# else:
+  # print("no")
+
+
+# a = int(input("enter a number: "))
+
+# if a == 1:
+  #  print("monday")
+
+# elif a == 2:
+  #  print("tuesday")
+
+# elif a == 3:
+  #  print("wednesday")
+
+# elif a == 4:
+ #   print("thursday")
+
+# elif a == 5:
+ #   print("friday")    
+
+# elif a == 6:
+  #  print("saturday")
+
+# else:
+  # print("invalid")    
+
+
