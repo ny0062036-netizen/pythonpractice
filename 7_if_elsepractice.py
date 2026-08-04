55c9bd2c84a433e4beb245d6566ab872315bb7db
nikku <ny0062036@gmail.com>
2026-08-04 11:41:52 +0530

    Add files via upload

---
+++ 7_if_elsepractice.py
@@ -0,0 +1,86 @@
+# a = int(input("enter a number: "))
+
+# if a == 1:
+  #  print("janurary")
+
+# elif a == 2:
+ #   print("february")
+
+# elif a == 3:
+  #  print("march")
+
+# elif a == 4:
+  #  print("april")
+
+# elif a == 5:
+  #  print("may")
+
+# elif a == 6:
+  #  print("june")
+
+# elif a == 7:
+  #  print("july")
+
+# elif a == 8:
+  #  print("august")
+
+# elif a == 9:
+  #  print("september")
+
+# elif a == 10:
+   # print("october")
+
+# elif a == 11:
+  #  print("november")
+
+# elif a == 12:
+  #  print("december")
+
+# else:
+  #  print("invalid")
+
+# a = int(input("enter a number: "))
+
+# match a:
+
+  # case 1:
+    # print("sunday")
+
+ # case 2:
+  #  print("monday")
+
+ # case 3:
+  #  print("tuesday")
+
+ # case 4:
+  #  print("wednesday")
+
+ # case 5:
+  #  print("thursday")
+
+ # case 6: 
+  #  print("friday")
+
+ # case 7:
+  #  print("saturday")
+
+ # case _:
+  #  print("invalid number)
+
+
+# ch = input("enter a character: ")
+
+# if 'A' <= ch <= 'Z':
+  # print("uppercase")
+# else:
+  # print("not uppercase")
+
+
+# num = int(input("enter a number: "))
+
+# if 100 <= abs(num) <= 999:
+  #  print("three digit number")
+# else:
+  #  print("not a three digit number")
+
+
