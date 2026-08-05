27c95b119e083ddfe2375062e123b168b0cb7021
nikku <ny0062036@gmail.com>
2026-08-05 11:59:32 +0530

    Add files via upload

--- whileloop.py
+++ whileloop.py
@@ -115,5 +115,84 @@ now counter value 0-1
 
 print -1 
 
-in this case no small value found it's mean this loop is infinite loop 
+in this case no small value found it's mean this loop is infinite loop
+
+
+
+counter is 0 
+
+0 is small than 5 
+
+print 0 
+
+counter value 0+1 is 1
+
+1 is small than 5 
+
+print 1 
+
+counter value 1+1 is 2
+
+2 is small than 5 
+
+print 2 
+
+counter value 2+1 is 3 
+
+3 is small 
+
+print 3 
+
+counter value 3+1 is 4 
+
+4 is small 
+
+print 4 
+
+conter value 4+1 is 5 
+
+counter value is 5 
+
+5 is not greater than 5 now value false
+
+
+
+counter is 0 
+
+counter value 0+1 is 1 
+
+1 is small 
+
+print 1 
+
+counter value 1+1 is 2 
+
+2 is small 
+
+print 2 
+
+counter value 2+1 is 3 
+
+3 is small 
+
+print 3 
+
+counter value 3+1 is 4 
+
+4 is small 
+
+print 4 
+
+counter value 4+1 is 5 
+
+5 is equal to 5 
+
+print 5 
+
+counter value 5+1 is 6 
+
+6 is greater this is false value 
+
+
+ 
 
