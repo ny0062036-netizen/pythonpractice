1acd106e9ecae5ff8beeab93ac1b691c2d516e90
nikku <ny0062036@gmail.com>
2026-08-05 11:45:39 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,119 @@
+counter = 1
+
+while counter <= 5:
+    print(counter)
+    counter += 1
+
+
+counter value is 1 and 1 is smaller than 5
+
+
+so counter print is 1
+
+and then counter is 1 and 1+1 is 2 
+
+so counter print 2
+
+
+and then counter value is 2+1 is 3 
+
+so counter print 3 
+
+in next case value is 3 and 3+1 is 4 
+
+counter print 4 
+
+now counter value is 4+1 is 5 and 5 is = to 5 
+
+
+counter print 5 
+
+counter value is 5+1 is 6 and 6 is larger than 5 so now value is false 
+
+
+
+
+counter = 8 
+
+8 is less than 9 
+
+print 8 
+
+counter value is 8+1 is 9 
+
+9 is = to 9 
+
+counter print 9 
+
+counter value is 9+1 is 10 
+
+10 is greater than 9 so now false 
+
+
+
+counter value is 8 
+
+
+8 is smaller than 9 
+
+print 8 
+
+counter value is 8-1 is 7 
+
+7 is smaller than 9
+
+print 7 
+
+
+counter value 7-1 is 6 
+
+6 is smaller than 9
+
+print 6
+
+counter value 6-1 
+
+5 is smaller than 9
+
+print 5 
+
+
+counter value 5-1 is 4 
+
+4 is small than 9 
+
+print 4 
+
+
+counter value 4-1 is 3 
+
+3 is small 
+
+print 3 
+
+counter value 3-1 
+
+2 is small 
+
+print 2 
+
+counter value 2-1 
+
+1 is small 
+
+print 1 
+
+counter value 1-1 
+
+0 is small 
+
+print 0 
+
+now counter value 0-1
+
+-1 is small 
+
+print -1 
+
+in this case no small value found it's mean this loop is infinite loop 
+
