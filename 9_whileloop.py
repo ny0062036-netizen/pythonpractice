505615efefcab1c9d489c9c2dd18a06a600548cf
nikku <ny0062036@gmail.com>
2026-08-05 13:13:42 +0530

    Add files via upload

---
+++ 9_whileloop.py
@@ -0,0 +1,119 @@
+# while True:
+#    print("hello world1")
+#    print("hello world2")
+#    print("hello world3")
+
+# counter = True
+
+# while counter:
+#  print("hello")
+# counter = False
+
+# counter = 5
+
+# while counter >= 0:
+#    print(counter)
+#    counter = counter - 1
+
+# counter = 1
+
+# while counter <= 5:
+#  print(counter)
+#    counter += 1 
+
+
+# counter = 8
+
+# while counter <= 9:
+#    print(counter)
+#    counter = counter + 1
+    
+
+# counter = 8
+
+# while counter <= 9:
+#    print(counter)
+#    counter = counter - 1
+    
+
+# counter = 0
+
+# while counter < 5:
+#    print(counter)
+#    counter += 1
+
+# counter = 0
+
+# while counter < 5:
+#    counter += 1
+#    print(counter)
+    
+
+# counter = 1
+
+# while counter <= 10:
+#    print(counter)
+#    counter += 1
+
+# counter = 50 
+
+# while counter >= 1:
+#    print(counter)
+#    counter -= 1
+
+# counter = 100
+
+# while counter >= 1:
+#   print(counter)
+#    counter -= 1
+
+# counter = 100
+
+# while counter >= 0:
+#    print(counter)
+#    counter -= 1  
+
+# counter = 1
+
+# while counter <= 100:
+#    print(counter)
+#    counter += 1
+
+
+# counter = -1
+
+# while counter >= -50:
+#    print(counter)
+#    counter -= 1
+
+# counter = -50
+
+# while counter <= -1:
+#    print(counter)
+#    counter += 1
+
+# counter = 10 
+
+# while counter <= 20:
+#    print(counter)
+#    counter += 1
+
+# counter = 2
+
+# while counter <= 20:
+#    print(counter)
+#    counter += 2
+
+# counter = 1
+
+# while counter <= 10:
+#    print(counter * 3)
+#    counter += 1
+
+# number = int(input("enter a number: "))
+
+# i = 1
+# while i <= number:
+#    print(i)
+#    i += 1
+
