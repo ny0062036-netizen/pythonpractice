a9ec29139d6e895adedd008f4544d7e0d709c788
nikku <ny0062036@gmail.com>
2026-08-05 10:54:25 +0530

    Add files via upload

---
+++ 8_matches.py
@@ -0,0 +1,179 @@
+# 1, monday
+# 2, tuesday
+# 3, wednesday
+# 4, thursday
+# 5, friday
+# 6, saturday
+# 7, sunday
+# .., invalid number
+
+# a = int(input("enter a number: "))
+
+# if a == 1:
+  #  print("monday")
+
+# elif a == 2:
+  #  print("tuesday")
+
+# elif a == 3:
+  #  print("wednesday")
+
+# elif a == 4:
+ #   print("thursday")
+
+# elif a == 5:
+ #   print("friday")    
+
+# elif a == 6:
+  #  print("saturday")
+
+# else:
+  # print("invalid")    
+
+
+###### MATCH ???:
+
+#      case  1:
+#            ----
+#            ----
+
+#      case 2:
+#           ----
+#           ----
+
+# char = input("enter light color: ")
+
+# match char:
+  #   case "red":
+   #     print("stop")
+
+# match char:
+  #  case "yellow":
+   #     print("wait")
+
+# match char:
+  #  case "green":
+   #     print("go")
+
+
+# char = input("enter only 1 character: ")
+
+# match char:
+
+  #  case "a":
+   #     print("vowel")
+
+   # case "e":
+    #    print("vowel")
+
+   # case "i":
+    #    print("vowel")
+
+   # case "o":
+    #    print("vowel")
+
+   # case "u":
+    #    print("vowel")
+
+   # case "-":
+    #    print("consonant")  
+
+# a = int(input("enter a number: "))
+
+# match a:
+
+  # case 1:
+    # print("sunday")
+
+ # case 2:
+  #  print("monday")
+
+ # case 3:
+  #  print("tuesday")
+
+ # case 4:
+  #  print("wednesday")
+
+ # case 5:
+  #  print("thursday")
+
+ # case 6: 
+  #  print("friday")
+
+ # case 7:
+  #  print("saturday")
+
+ # case _:
+  #  print("invalid number)
+
+
+# amount = int(input("enter amount: "))
+
+
+# match amount:
+   
+  # case 20:
+   #    print("200ml")
+
+  # case 40:
+   #     print("750ml")
+
+   # case 50:
+    #    print("1ltr")    
+
+
+# month = int(input("enter month number: "))
+
+# match month:
+  #  case 1:
+   #     print("31 days")
+
+   # case 2:
+    #    print("29 days")    
+
+   # case 3:
+    #    print("31 days")  
+      
+   #  case 4:
+    # print("30 days")    
+
+   # case 5:    
+    #    print("31 days")
+
+    # case 6:
+   #    print("30 days")
+
+   # case 7:
+    #    print("31 days")    
+
+   # case 8:
+    #    print("31 days")    
+
+   # case 9:
+    #    print("30 days")    
+
+   # case 10:
+    #    print("31 days")    
+
+   # case 11:
+    #    print("30 days")    
+
+   # case 12:
+    #    print("31 days")    
+
+
+# char = input("enter light color: ")
+
+# if char == "red":
+  #    print("stop")
+
+# elif char == "yellow":
+  #  print("ready")
+
+# elif char == "green":
+  #   print("go")
+
+# else:
+  #   print("not defined")
+
+
