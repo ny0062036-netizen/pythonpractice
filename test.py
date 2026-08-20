906e6fb9e6beade42d9dbaa02a111be2270a8b8e
nikku <ny0062036@gmail.com>
2026-08-20 12:08:10 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+# for x in range (1,21):
+#     if x % 5 != 0:
+#       print(x)
+
+# for i in range(0,21):
+#   if i % 2 != 0:
+#      print(i)
+
+# data = input("enter your password: ")
+# data = "test@123"
+
+# if len(data) >= 8:
+#     if '0' in data or "1" in data or  "2" in data or "3" in data or "4" in data or "5" in data or "6" in data or "7" in data or "8" in data or "9" in data:
+#         if '#' in data or '$' in data or '@' in data:
+#             print('valid password')
+#         else:
+#             print("missing $#@ in the password")
+#     else:
+#         print('your password does not have a number.')
+# else:
+#     print("not a correct password it is less than 8 char.")
+
+# email = '  ny0062036@gmail.com   '
+# print(email.strip())
+
+# url = 'https://xyz.com'
+# print(url.startswith('https') )
+
+# url = 'https://xyz.com'
+# print(url.endswith('https'))
+
