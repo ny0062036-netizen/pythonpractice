a2783ab5efcf92449d0c6db35155fe95ea8f4702
lyfofnikesh <ny0062036@gmail.com>
2026-08-23 16:52:03 +0530

    Add files via upload

--- 10_jump_statements.py
+++ 10_jump_statements.py
@@ -54,3 +54,20 @@
 #    if item == "stop":
 #        break
 
+# while True:
+#     item = input("enter your item: ")
+#     if item == "stop":
+#         break
+
+# num = 7
+# i = 2
+
+# while i <= num-1:
+
+#    if num % i == 0:
+#        print("not a prime number")
+#    i += 1  
+
+#    if i == num:
+#      print("prime")
+  
